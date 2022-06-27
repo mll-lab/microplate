546b6c2bea28f631ebac4e9939dc37e261c0cea3
Benedikt Franke <benedikt.franke@mll.com>
2022-06-27 15:13:42 +0200

    Support `illuminate/support` 9 (#14)

--- rector.php
+++ rector.php
@@ -1,22 +1,14 @@
 <?php declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::PHP_74);
+return static function (RectorConfig $config): void {
+    $config->services()->set(AddClosureReturnTypeRector::class);
+    $config->import(SetList::CODE_QUALITY);
+    $config->import(SetList::PHP_74);
 
-    $parameters = $containerConfigurator->parameters();
-
-    // paths to refactor; solid alternative to CLI arguments
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
-
-    // is your PHP version different from the one your refactor to? [default: your PHP version], uses PHP_VERSION_ID format
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
-
-    // Path to phpstan with extensions, that PHPSTan in Rector uses to determine types
-    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, getcwd() . '/phpstan-for-config.neon');
+    $config->paths([__DIR__ . '/src', __DIR__ . '/tests']);
+    $config->phpVersion(PhpVersion::PHP_74);
 };
